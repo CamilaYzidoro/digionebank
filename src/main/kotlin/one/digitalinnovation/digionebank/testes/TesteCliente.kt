cf57ad14ba653b9592656500676d3ecbbcd0471a
CamilaYzidoro <camila.casagrandey@gmail.com>
2021-04-21 11:59:38 -0300

    Created client class and test

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteCliente.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Cliente
+import one.digitalinnovation.digionebank.ClienteTipo
+
+fun main{
+    val gabriela = Cliente(
+        nome = "Gabriela Oliveira",
+        cpf="987.321.600-54",
+        clienteTipo = ClienteTipo.PF,
+        senha="042021"
+    )
+
+    println(gabriela)
+
+    TesteAutenticacao().autentica(gabriela)
+}
