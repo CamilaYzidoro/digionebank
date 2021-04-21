5b60235dd7703b2ea6d889966d3c547c1bf7ec14
CamilaYzidoro <camila.casagrandey@gmail.com>
2021-04-21 11:58:44 -0300

    updated employee test

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -1,11 +1,12 @@
 package one.digitalinnovation.digionebank.testes
 
-import one.digitalinnovation.digionebank.Funcionario
 import one.digitalinnovation.digionebank.Gerente
 
 
 fun main() {
-    val maria = Gerente(nome="Maria Claudia", cpf="456.789.001-23", salario=5000.0)
+    val maria = Gerente(nome="Maria Claudia", cpf="456.789.001-23", salario=5000.0, senha="2104")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
