e030334652441f02623c3ad7eacb79c7d144c8af
CamilaYzidoro <camila.casagrandey@gmail.com>
2021-04-21 09:43:34 -0300

    Update to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Camila"
-    var cpf: String = "123.456.789-00"
-    private set //Atribuir valores apenas na classe, não deixará alterar em outros lugares do código.
-
-    constructor() //Contrutor secundário
-
-    fun pessoaInfo()= "$nome e $cpf"
-}
-
-fun main() {
-    val camila = Pessoa()
-    println(camila.pessoaInfo())
+abstract class Pessoa {
+     val nome: String,
+     val cpf: String
 }
